6f4d7a78380e7c8f57e367f7772589c075b76dc1
Gamer2082 <revolutiongr4@gmail.com>
2023-07-13 12:45:33 +0000

    up

--- src/main/java/fr/ascaria/ascacore/Ascacore.java
+++ src/main/java/fr/ascaria/ascacore/Ascacore.java
@@ -7,7 +7,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ascacore extends JavaPlugin {
     public static Ascacore instance;
-    public static Ascacore on;
 
     @Override
     public void onEnable() {
